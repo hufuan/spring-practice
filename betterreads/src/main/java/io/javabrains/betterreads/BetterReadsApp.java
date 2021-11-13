501f5f4ad1390c81738e12fc6e58da4ddf4ef721
hufuan <fuan_707@163.com>
2021-11-13 22:30:50 +0800

    test

--- betterreads/src/main/java/io/javabrains/betterreads/BetterReadsApp.java
+++ betterreads/src/main/java/io/javabrains/betterreads/BetterReadsApp.java
@@ -1,9 +1,12 @@
 package io.javabrains.betterreads;
 
-import com.fasterxml.jackson.core.JacksonException;
+
 import io.javabrains.betterreads.author.Author;
 import io.javabrains.betterreads.author.AuthorRepository;
+import io.javabrains.betterreads.book.Book;
+import io.javabrains.betterreads.book.BookRepository;
 import io.javabrains.betterreads.connection.DataStaxAstraProperties;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -26,6 +34,9 @@ import java.util.stream.Stream;
 public class BetterReadsApp {
     @Autowired
     AuthorRepository authorRepository;
+
+    @Autowired
+    BookRepository bookRepository;
 
     @Value("${datadump.location.author}")
     private String authordumpLocation;
@@ -62,6 +73,71 @@ public class BetterReadsApp {
 
     private void initWorks() {
         Path path = Paths.get(worksdumpLocation);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        int number = 0;
+        try (Stream <String> lines = Files.lines(path)){
+            lines.limit(9).forEach(line->{
+                String jsonString = line.substring(line.indexOf("{"));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    //construct Book object
+                    Book book = new Book();
+                    //0. book id
+                    book.setId(jsonObject.optString("key").replace("/works/", ""));
+                    //1. book name
+                    book.setName(jsonObject.optString("title"));
+
+                    //2. book description
+                    JSONObject descriptionObj = jsonObject.optJSONObject("description");
+                    if (descriptionObj != null){
+                        book.setDescription(descriptionObj.optString("value"));
+                    }
+                    //3. publish time
+                    JSONObject publishedObj = jsonObject.optJSONObject("created");
+                    if (publishedObj != null){
+                        String dataStr = publishedObj.getString("value");
+                        //System.out.println("data value: " + dataStr);
+                        book.setPublishedDate(LocalDate.parse(dataStr, dtf));
+                    }
+                    //4. cover ids
+                    JSONArray coversJSONArr = jsonObject.optJSONArray("covers");
+                    if (coversJSONArr != null){
+                        List<String> coverIds = new ArrayList<>();
+                        for (int i = 0; i < coversJSONArr.length(); i++){
+                            coverIds.add(coversJSONArr.getString(i));
+                        }
+                        book.setCoverIds(coverIds);
+                    }
+                    //5. authors
+                    JSONArray authorJSONArr = jsonObject.optJSONArray("authors");
+                    if (authorJSONArr != null) {
+                        List<String> authorIds = new ArrayList<>();
+                        for (int i = 0; i < authorJSONArr.length();i ++){
+                            String authorId = authorJSONArr.getJSONObject(i).getJSONObject("author").getString("key")
+                                    .replace("/authors/", "");
+                            authorIds.add(authorId);
+                        }
+                        //5.1 author id
+                        book.setAuthorIds(authorIds);
+                        List<String> authorNames = authorIds.stream().map(id->authorRepository.findById(id))
+                            .map(optionAuthor->{
+                                if (!optionAuthor.isPresent()){
+                                    return "Unknown Author";
+                                }
+                                return optionAuthor.get().getName();
+                            }).collect(Collectors.toList());
+                        //5.1 author name
+                        book.setAuthorNames(authorNames);
+                    }
+                    System.out.println("save book: " + book.getName());
+                    bookRepository.save(book);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @PostConstruct
@@ -76,7 +152,7 @@ public class BetterReadsApp {
         System.out.println(authordumpLocation);
         System.out.println(worksdumpLocation);
         //initAuthor();
-        //initWorks();
+        initWorks();
     }
 
     @Bean
